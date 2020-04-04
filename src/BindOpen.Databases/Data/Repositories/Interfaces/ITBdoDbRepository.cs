930151156d2ce5a90e69c020b7a8b1c071370f89
xgonier <xgonier@meltingsoft.com>
2020-04-04 18:53:50 +0200

    - Updating BDO packages.

--- src/BindOpen.Databases/Data/Repositories/Interfaces/ITBdoDbRepository.cs
+++ src/BindOpen.Databases/Data/Repositories/Interfaces/ITBdoDbRepository.cs
@@ -1,5 +1,8 @@
 ﻿using BindOpen.Application.Services;
+using BindOpen.Data.Connections;
 using BindOpen.Databases.Data.Models;
+using BindOpen.System.Diagnostics;
+using System;
 
 namespace BindOpen.Databases.Data.Repositories
 {
@@ -15,5 +18,20 @@ namespace BindOpen.Databases.Data.Repositories
         {
             get;
         }
+
+        /// <summary>
+        /// Executing the specified action during a new connection.
+        /// </summary>
+        /// <param name="action">The action to execute.</param>
+        /// <param name="isAutoConnected">Indicates whether the connection must be automatically connected.</param>
+        new void UsingConnection(Action<IBdoDbConnection> action, bool isAutoConnected = true);
+
+        /// <summary>
+        /// Executing the specified action during a new connection.
+        /// </summary>
+        /// <param name="action">The action to execute.</param>
+        /// <param name="log">The log to consider.</param>
+        /// <param name="isAutoConnected">Indicates whether the connection must be automatically connected.</param>
+        new void UsingConnection(Action<IBdoDbConnection, IBdoLog> action, IBdoLog log, bool isAutoConnected = true);
     }
 }
